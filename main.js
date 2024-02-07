56881c39d7a79577e8094620f49ebddd9f3fc98c
DjeoR <75511446+DjeoR@users.noreply.github.com>
2024-02-07 16:36:44 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-let SIZE;
-let CELL_SIZE;
-let RADIUS = 3;
-let TYPE = "circle";
-let ALIVEFROM;
-let ALIVETO;
-let SPAWNFROM;
-let SPAWNTO;
 let gridData;
 let newGridData;
+
+let settings = {
+    DENSITY: 0,
+    SIZE: 0,
+    SPEED: 0,
+    RADIUS: 0,
+    TYPE: "circle",
+    LIVEFROM: 0,
+    LIVETO: 0,
+    SPAWNFROM: 0,
+    SPAWNTO: 0
+}
 
 let playing = false;
 let stepID;
@@ -16,24 +20,21 @@ const canvas = document.getElementById("canvas");
 
 const ctx = canvas.getContext("2d");
 
-console.log(canvas);
-
+updateSettings();
 randomGrid();
 draw();
-ctx.fillRect(10, 10, 10, 10);
 
 function randomGrid() {
 
-    SIZE = Number(document.getElementById("size").value);
-    CELL_SIZE = 600 / SIZE;
+    settings.SIZE = Number(document.getElementById("size").value);
 
-    gridData = new Array(SIZE);
+    gridData = new Array(settings.SIZE);
 
-    for (let i = 0; i < SIZE; i++) {
-        gridData[i] = new Array(SIZE);
+    for (let i = 0; i < settings.SIZE; i++) {
+        gridData[i] = new Array(settings.SIZE);
 
-        for (let j = 0; j < SIZE; j++) {
-            gridData[i][j] = Math.random() > 0.5 ? 0 : 1;
+        for (let j = 0; j < settings.SIZE; j++) {
+            gridData[i][j] = 100 * Math.random() > settings.DENSITY ? 0 : 1;
         }
     }
 }
@@ -41,12 +42,35 @@ function randomGrid() {
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    for (let i = 0; i < SIZE; i++) {
-        for (let j = 0; j < SIZE; j++) {
+    for (let i = 0; i < settings.SIZE; i++) {
+        for (let j = 0; j < settings.SIZE; j++) {
             ctx.fillStyle = gridData[i][j] == 1 ? "lightgray" : "black";
-            ctx.fillRect(i * CELL_SIZE, j * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+            ctx.fillRect(i * 600 / settings.SIZE, j * 600 / settings.SIZE, 600 / settings.SIZE, 600 / settings.SIZE);
         }
     }
+}
+
+function updateSettings() {
+
+    pause();
+
+    settings.DENSITY = Number(document.getElementById("density").value);
+    settings.TYPE = document.getElementById("k1type").checked ? "circle" : "square";
+    settings.RADIUS = Number(document.getElementById("k1radius").value);
+    settings.LIVEFROM = Number(document.getElementById("k1livefrom").value);
+    settings.LIVETO = Number(document.getElementById("k1liveto").value);
+    settings.SPAWNFROM = Number(document.getElementById("k1spawnfrom").value);
+    settings.SPAWNTO = Number(document.getElementById("k1spawnto").value);
+
+    document.getElementById("densitydisplay").innerText = settings.DENSITY + "%";
+    document.getElementById("sizedisplay").innerText = document.getElementById("size").value + "px";
+    document.getElementById("speeddisplay").innerText = Math.round(1000 / (1010 - 10 * settings.SPEED)) + "fps";
+    document.getElementById("k1radiusdisplay").innerText = settings.RADIUS + "px";
+}
+
+function updateSpeed() {
+    settings.SPEED = 1010 - 10 * Number(document.getElementById("speed").value);
+    document.getElementById("speeddisplay").innerText = Math.round(1000 / settings.SPEED) + "fps";
 }
 
 function reset() {
@@ -57,22 +81,12 @@ function reset() {
 
 function step() {
 
-    newGridData = new Array(SIZE);
+    newGridData = new Array(settings.SIZE);
 
-    console.log(document.getElementById("kernel1radius").value);
-    RADIUS = Number(document.getElementById("kernel1radius").value);
-    TYPE = document.getElementById("kernel1circle").checked ? "circle" : "square";
-    ALIVEFROM = Number(document.getElementById("kernel1alivefrom").value);
-    ALIVETO = Number(document.getElementById("kernel1aliveto").value);
-    SPAWNFROM = Number(document.getElementById("kernel1spawnfrom").value);
-    SPAWNTO = Number(document.getElementById("kernel1spawnto").value);
+    for (let i = 0; i < settings.SIZE; i++) {
+        newGridData[i] = new Array(settings.SIZE);
 
-    console.log(RADIUS);
-
-    for (let i = 0; i < SIZE; i++) {
-        newGridData[i] = new Array(SIZE);
-
-        for (let j = 0; j < SIZE; j++) {
+        for (let j = 0; j < settings.SIZE; j++) {
             updateCell(i, j);
         }
     }
@@ -83,10 +97,10 @@ function step() {
 
 function updateCell(x, y) {
     
-    let count = getKernelCellCount(x, y, RADIUS, TYPE);
-    if (count < ALIVEFROM || count > ALIVETO) {
+    let count = getKernelCellCount(x, y, settings.RADIUS, settings.TYPE);
+    if (count < settings.LIVEFROM || count > settings.LIVETO) {
         newGridData[x][y] = 0;
-    } else if (count >= SPAWNFROM && count <= SPAWNTO) {
+    } else if (count >= settings.SPAWNFROM && count <= settings.SPAWNTO) {
         newGridData[x][y] = 1;
     } else {
         newGridData[x][y] = gridData[x][y];
@@ -119,10 +133,10 @@ function getKernelCellCount(x, y, radius, type) {
 }
 
 function wrap(value) {
-    return (value + SIZE) % SIZE;
+    return (value + settings.SIZE) % settings.SIZE;
 }
 
-function play_pause() {
+function playPause() {
     if (!playing) {
         play();
     } else {
@@ -143,7 +157,7 @@ function pause() {
 
 function step_repeating() {
     if (playing) {
-        setTimeout(step_repeating, 500 - (document.getElementById("speed").value * 4.95));
+        setTimeout(step_repeating, settings.SPEED);
         step();
     }
 }
